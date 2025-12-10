13e51afd9d416d9092364cf986ac9a43e50f2090
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-12-10 22:03:48 +0300

    Создал модель кафедры, сделал метод подгрузки из БД

---
+++ Models/DepartmentModel.cs
@@ -0,0 +1,57 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace dotnet_lab11.Models;
+
+public class DepartmentModel
+{
+	private static readonly string _selectDepartmentsCommand = @"SELECT [departmentid], [name], [facultyname] FROM [dbo].[Departments]";
+	private static readonly string _insertDepartmentCommand = @"INSERT INTO [dbo].[Departments] ([name], [facultyname]) VALUES (@Name, @FacultyName)";
+	private static readonly string _updateDepartmentCommand = @"UPDATE [dbo].[Departments] SET [name] = @Name, [facultyname] = @FacultyName WHERE [departmentid] = @Id";
+	private static readonly string _deleteDepartmentCommand = @"DELETE FROM [dbo].[Departments] WHERE [departmentid] = @Id";
+
+	public Int16 Id { get; set; } = -1;
+
+	public string Name { get; set; } = "";
+
+	public string? FacultyName { get; set; } = null;
+
+	/// <summary> Получение списка кафедр </summary>
+	/// <param name="connection">Соединение с сервером</param>
+	/// <returns>Список кафедр</returns>
+	public static List<DepartmentModel> List(SqlConnection connection)
+	{
+		List<DepartmentModel> list = [];
+
+		using SqlCommand comm = new();
+
+		try {
+			comm.Connection = connection;
+			comm.CommandText = _selectDepartmentsCommand;
+			comm.CommandType = System.Data.CommandType.Text;
+
+			connection.Open();
+			var reader = comm.ExecuteReader();
+			while (reader.Read()) {
+				var newDepartment = new DepartmentModel()
+				{
+					Id = (Int16)reader["departmentid"],
+					Name = (String)reader["name"],
+					FacultyName = (String)reader["facultyname"]
+				};
+
+				list.Add(newDepartment);
+			}
+		}
+		finally {
+			if (connection is not null &&
+				connection.State == System.Data.ConnectionState.Open) {
+				connection.Close();
+			}
+		}
+
+		return list;
+	}
+
+
+}
